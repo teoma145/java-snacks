c7eb59979994ea226b79551ddfbf8f1e0f1733db
teoma145 <matteodark145@yahoo.it>
2024-03-28 16:30:52 +0100

    snack bonus 1

---
+++ src/org/snacks/SecondBonus.java
@@ -0,0 +1,18 @@
+package org.snacks;
+
+import java.util.Scanner;
+
+public class SecondBonus {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Quanti secondi vuoi convertire");
+        int secondi = scan.nextInt();
+        int hours = secondi/(60*60);
+        int secondiRimasti = secondi % (60*60);
+        int minuti = secondiRimasti / 60;
+        int secondiFinali = secondiRimasti % 60;
+        System.out.println(hours+":"+minuti+":"+secondiFinali);
+
+
+    }
+}
